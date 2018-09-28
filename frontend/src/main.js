54dbf8f3ba728d9c10f65d6317713c7f02a88f7c
Jörn Willhöft <j.willhoeft@willhoeft-it.com>
2018-09-28 17:47:25 +0200

    removed yet unused "router" plugin removed example components and icons removed unused js library parse-xsd-duration.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,9 @@ import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
-import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
