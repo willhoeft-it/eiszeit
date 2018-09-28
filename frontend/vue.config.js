7a74b967d5dbba1d3d906d3ec0448b25fa50e1d7
Jörn Willhöft <j.willhoeft@willhoeft-it.com>
2018-09-28 11:19:09 +0200

    dev basex proxy configuration

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/timetracking/static/'
-    : '/'
+  baseUrl: process.env.NODE_ENV === 'production' ? '/timetracking/static/'  : '/',
+  // development proxy to running basex API
+  devServer: {
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:8984/timetracking/',
+      }
+    }
+  }
     /*
     chainWebpack: config => {
     // javascript libraries that are not available via npm and linked by script tag in index.html. Files in /public folder
