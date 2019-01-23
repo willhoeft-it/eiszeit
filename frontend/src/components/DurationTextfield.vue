e97765631cce137f36e18dbe7bbf80129bd9cc34
Jörn Willhöft <j.willhoeft@willhoeft-it.com>
2019-01-23 17:01:47 +0100

    minor code cleanup

--- frontend/src/components/DurationTextfield.vue
+++ frontend/src/components/DurationTextfield.vue
@@ -16,15 +16,15 @@
   export default Vue.component('duration-textfield', {
     mixins: [dateUtils],
     props: ['value'],
-      methods: {
-        // Instead of updating the value directly, this
-        // method is used to format and place constraints
-        // on the input's value
-        updateValue: function (event) {
-          // Emit the number value through the input event
-          const value = event.target.value
-          this.$emit('input', this.hoursAsDuration(value))
-        }
+    methods: {
+      // Instead of updating the value directly, this
+      // method is used to format and place constraints
+      // on the input's value
+      updateValue: function (event) {
+        // Emit the number value through the input event
+        const value = event.target.value
+        this.$emit('input', this.hoursAsDuration(value))
       }
+    }
   })
 </script>
