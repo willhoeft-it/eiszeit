2ae39a001fad75003a4ca337abe030c9a1a76617
Jörn Willhöft <j.willhoeft@willhoeft-it.com>
2019-02-15 20:50:21 +0100

    On large screens date pickers change to enhanced text fields

--- frontend/src/components/DailyTimePicker.vue
+++ frontend/src/components/DailyTimePicker.vue
@@ -1,5 +1,16 @@
 <template>
+
+  <v-text-field
+    v-if="$vuetify.breakpoint.mdAndUp"
+    v-bind:value="value"
+    @change="updateValueFromTextField"
+    @focus="$event.target.select()"
+    :label="label"
+    type="text" >
+  </v-text-field>
+
   <v-menu
+    v-else
     :close-on-content-click="false"
     v-model="menu"
     :nudge-right="40"
@@ -16,6 +27,7 @@
       :label="label"
       :append-icon="(value)?'clear':''"
       @click:append="onPickerInput('')"
+      type="text"
       readonly
     ></v-text-field>
     <!-- unfortunately whether time-picker nor its native root component emit an event if the same time is clicked again -->
@@ -41,10 +53,25 @@
     data: function() {
       return {
         menu: false,
+        innerValue: null,
         allowedMinutes: m => m % 15 === 0
       };
     },
     methods: {
+      // Turn various input formats into a valid time
+      updateValueFromTextField: function (v) {
+        const re = /^(\d*):?(\d\d)$/
+        if (re.test(v)) {
+          this.innerValue = v.replace(re, '$1:$2')
+          if (this.innerValue !== this.value) {
+            this.$emit('change', this.innerValue)
+          }
+        } else {
+          this.innerValue = '00:00'
+          this.$emit('change', this.innerValue)
+        }
+        this.$emit('input', this.innerValue)
+      },
       onPickerInput: function(newValue) {
         console.log("onPickerInput: ", newValue)
         if (newValue != this.value) {
