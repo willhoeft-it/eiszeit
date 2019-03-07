e60aa83c39fddf96551c91f74068b5993dcf886c
Jörn Willhöft <j.willhoeft@willhoeft-it.com>
2019-03-07 21:22:53 +0100

    removed the two outer divs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,84 +1,80 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <v-app>
-        <v-navigation-drawer v-model="drawer" fixed app >
-          <v-list dense>
-            <!--
-                <router-link to="/">Home</router-link> |
-                <router-link to="/about">About</router-link>
-            -->
-            <v-list-tile @click="page='homePage'">
-              <v-list-tile-action>
-                <v-icon>home</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>dashboard</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile @click="page='dailyBookingPage'">
-              <v-list-tile-action>
-                <v-icon>book</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>daily booking</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile @click="page='taskManagingPage'">
-              <v-list-tile-action>
-                <v-icon>create_new_folder</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>task managing</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile @click="page='reportProjectsPage'">
-              <v-list-tile-action>
-                <v-icon>list</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>report on projects</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile @click="page='reportWorkingtimePage'">
-              <v-list-tile-action>
-                <v-icon>list</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>report on working time</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-navigation-drawer>
-        <v-toolbar dark fixed app>
-          <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-          <v-toolbar-title>Timetracking</v-toolbar-title>
-        </v-toolbar>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" fixed app >
+      <v-list dense>
+        <!--
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        -->
+        <v-list-tile @click="page='homePage'">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="page='dailyBookingPage'">
+          <v-list-tile-action>
+            <v-icon>book</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>daily booking</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="page='taskManagingPage'">
+          <v-list-tile-action>
+            <v-icon>create_new_folder</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>task managing</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="page='reportProjectsPage'">
+          <v-list-tile-action>
+            <v-icon>list</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>report on projects</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="page='reportWorkingtimePage'">
+          <v-list-tile-action>
+            <v-icon>list</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>report on working time</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Timetracking</v-toolbar-title>
+    </v-toolbar>
 
-        <daily-booking-page v-if="page === 'dailyBookingPage'" v-on:pageMessageEvent="showMessage">
-        </daily-booking-page>
+    <daily-booking-page v-if="page === 'dailyBookingPage'" v-on:pageMessageEvent="showMessage">
+    </daily-booking-page>
 
-        <task-managing-page v-if="page === 'taskManagingPage'" v-on:pageMessageEvent="showMessage">
-        </task-managing-page>
+    <task-managing-page v-if="page === 'taskManagingPage'" v-on:pageMessageEvent="showMessage">
+    </task-managing-page>
 
-        <report-projects-page v-if="page === 'reportProjectsPage'" v-on:pageMessageEvent="showMessage">
-        </report-projects-page>
+    <report-projects-page v-if="page === 'reportProjectsPage'" v-on:pageMessageEvent="showMessage">
+    </report-projects-page>
 
-        <report-workingtime-page v-if="page === 'reportWorkingtimePage'" v-on:pageMessageEvent="showMessage">
-        </report-workingtime-page>
+    <report-workingtime-page v-if="page === 'reportWorkingtimePage'" v-on:pageMessageEvent="showMessage">
+    </report-workingtime-page>
 
-        <v-snackbar v-model="snackbar.show" :color="snackbar.level" :multi-line="snackbar.level === 'error'" bottom :timeout="(snackbar.level === 'error') ? 0 : 2000">
-        {{ snackbar.message }}
-          <v-btn flat @click="snackbar.show = false">
-            Close
-          </v-btn>
-        </v-snackbar>
-        <v-footer dark app>
-          <span class="white--text">&copy; 2019 - Willhöft IT-Beratung GmbH</span>
-        </v-footer>
-      </v-app>
-    </div>
-  </div>
+    <v-snackbar v-model="snackbar.show" :color="snackbar.level" :multi-line="snackbar.level === 'error'" bottom :timeout="(snackbar.level === 'error') ? 0 : 2000">
+    {{ snackbar.message }}
+      <v-btn flat @click="snackbar.show = false">
+        Close
+      </v-btn>
+    </v-snackbar>
+    <v-footer dark app>
+      <span class="white--text">&copy; 2019 - Willhöft IT-Beratung GmbH</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -91,7 +87,7 @@
   // eslint-disable-next-line
   import ReportWorkingtimePage from '@/views/ReportWorkingtimePage.vue'
 
-  // TODO: login 
+  // TODO: login
 
   export default {
     data: function() {
