854cf27129b165348edf0930864e29b7125ef213
Jörn Willhöft <j.willhoeft@willhoeft-it.com>
2019-06-17 22:14:32 +0200

    Added DashboardPage

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import DashboardPage from '@/views/DashboardPage.vue'
 import DailyBookingPage from '@/views/DailyBookingPage.vue'
 import TaskManagingPage from '@/views/TaskManagingPage.vue'
 import UserManagingPage from '@/views/UserManagingPage.vue'
@@ -16,7 +17,7 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
     { path: '/', component: null },
-    { path: '/dashboard', component: null },
+    { path: '/dashboard', component: DashboardPage },
     { path: '/dailyBooking', component: DailyBookingPage },
     { path: '/taskManaging', component: TaskManagingPage },
     { path: '/userManaging', component: UserManagingPage },
