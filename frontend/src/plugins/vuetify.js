53ce73da589f652e51907a351d9f2f4f6c6ee359
Jörn Willhöft <j.willhoeft@willhoeft-it.com>
2019-08-13 22:33:00 +0200

    Fixed fab icon align by correcting CSS order

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import 'vuetify/dist/vuetify.min.css'
 import 'typeface-roboto/index.css'
 
 Vue.use(Vuetify, {
